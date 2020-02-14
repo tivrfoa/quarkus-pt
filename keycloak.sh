9088983b23ac84ad3b087635b42fc488df0a955f
Leandro Coutinho <lescoutinhovr@hotmail.com>
2020-02-14 17:16:28 -0300

    testing SecurityIdentity

--- keycloak.sh
+++ keycloak.sh
@@ -4,3 +4,5 @@ docker pull jboss/keycloak
 docker run --name keycloak -p 8180:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin jboss/keycloak
 curl localhost:8180
 
+# After adding quarkus-realm.json, it is necessary to change
+# "Valid Redirect URIs" and "Web Origns" in "backend-service" settings.
--- src/main/java/org/acme/quickstart/keycloak/ComputadoresResource.java
+++ src/main/java/org/acme/quickstart/keycloak/ComputadoresResource.java
@@ -1,6 +1,7 @@
 package org.acme.quickstart.keycloak;
 
 import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,12 +13,17 @@ import org.eclipse.microprofile.openapi.annotations.security.OAuthFlows;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 
+import io.quarkus.security.identity.SecurityIdentity;
+
 @Path("/computadores")
 @SecurityScheme(securitySchemeName = "quarkus-oauth",
         type = SecuritySchemeType.OAUTH2,
         flows = @OAuthFlows(password = 
                 @OAuthFlow(tokenUrl = "http://localhost:8180/auth/realms/quarkus/protocol/openid-connect/token")))
 public class ComputadoresResource {
+
+    @Inject
+    SecurityIdentity keyCloakSecurityContext;
 
     @GET
     @Path("/publico")
@@ -32,6 +38,7 @@ public class ComputadoresResource {
     @SecurityRequirement(name = "quarkus-oauth")
     @Produces(MediaType.TEXT_PLAIN)
     public String protegido() {
-        return "hello";
+        return "hello " + keyCloakSecurityContext.getRoles()
+                + ", principal name: " + keyCloakSecurityContext.getPrincipal().getName();
     }
 }
